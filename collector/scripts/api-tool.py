a5d757646e8f3cffe18620cbc4bc369540b6117a
tatsui <2360691+tatsu-i@users.noreply.github.com>
2020-08-04 00:48:43 +0900

    first commit

--- collector/scripts/api-tool.py
+++ collector/scripts/api-tool.py
@@ -1,6 +1,7 @@
 import os
 import time
 import fire
+import yaml
 import tweepy
 
 class Cli:
@@ -20,13 +21,22 @@ class Cli:
             print(f"[{twilist.slug}]")
             print("full_name="+twilist.full_name)
 
-    def get_list_member_ids(self, listname, screen_name):
+    def get_list_member_ids(self, listname, screen_name, config_file="/conf/config.yaml"):
+        conf = {}
+        if os.path.exists(config_file):
+            with open(config_file) as f:
+                conf = yaml.safe_load(f)
+        conf["list"] = conf.get("list", {})
+        conf["list"][listname] = conf["list"].get(listname, [])
         for member in tweepy.Cursor(self.api.list_members,slug=listname, owner_screen_name=screen_name).items():
             if not member.following and not member.protected:
                 member.follow()
                 time.sleep(5)
+            conf["list"][listname].append({"id":str(member.id), "name": member.screen_name})
             print (f'"{member.id}", # {member.screen_name}')
-            time.sleep(0.3)
+            time.sleep(0.1)
+        with open(config_file, "w") as f:
+            yaml.dump(conf, f)
 
     def get_id(self, screen_name):
         user = self.api.get_user(screen_name) 
